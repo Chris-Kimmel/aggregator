0ed4016546a1f70f7b1e12df9db31d52ee8a579d
Chris Kimmel <kimmel.95@osu.edu>
2021-08-12 10:31:19 -0400

    Clean up test code

--- test/hiv/test.sh
+++
@@ -1,7 +0,0 @@
-./aggregator.py \
-    --pos-to-aggregate pos_0b \
-    --stat frac_below_lower_thresh \
-    --covg covg \
-    --region motif \
-    --pos-regions pos_0b \
-    test/files/to_aggregate.csv test/files/hiv_regions.csv wt_cellular_positive_data.csv
